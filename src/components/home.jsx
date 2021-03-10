a85ab46b8dffdcac07bdee6b355804b83afafab5
unknown <cholgwang2001124@gmail.com>
2021-03-11 02:51:07 +0300

    initial complete

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,8 +3,10 @@ export const Home = (props) => {
     <div id='about'>
       <div className='container'>
         <div className='row'>
-          <div className='col-xs-12 col-md-6'>
+          <div className='col-xs-12 col-md-5'>
             <div className='about-text'>
+              <br/>
+              <br/>
               <h2>GPU Compute.</h2>
               <h2>Affordable Cost.</h2>
               <br/>
@@ -13,7 +15,7 @@ export const Home = (props) => {
               <h3>Over-paying for compute cost is so 2020.</h3>
             </div>
           </div>
-          <div className='col-xs-12 col-md-6 intro'>
+          <div className='col-xs-12 col-md-7 intro'>
             <video autoPlay loop={true} className='video'>
               <source src="img/streamingapp.mp4" type="video/mp4"/>
             </video>
