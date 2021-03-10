81b5f67cad24db96926bdf04a281856606bb13a6
unknown <cholgwang2001124@gmail.com>
2021-03-11 02:53:52 +0300

    complete

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -4,30 +4,11 @@ export const Navigation = (props) => {
       <div className='container'>
 
         <div className='navbar-header'>
-          <button
-            type='button'
-            className='navbar-toggle collapsed'
-            data-toggle='collapse'
-            data-target='#bs-example-navbar-collapse-1'
-          >
-            {' '}
-            <span className='sr-only'>Toggle navigation</span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-          </button>
-
-          <a className='navbar-brand page-scroll' href='/'>
-            <img src='img/ARC_Compute.svg' style={{"width" : "100%", "marginTop" : "-35%"}} alt="logo" />
-          </a>{' '}
           <a className='navbar-brand page-scroll' href='/' style={{"color" : "#5b55b9"}}>
             A R C &nbsp; C O M P U T E
           </a>{' '}
         </div>
-        <div
-            className='collapse navbar-collapse'
-            id='bs-example-navbar-collapse-1'
-        >
+        <div>
           <ul className='nav navbar-nav navbar-right'>
             <li>
               <button
